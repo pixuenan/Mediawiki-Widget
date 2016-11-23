d32a5b0f60f3d085953e3c3a7ef2cd858f61c8ca
pishijiazu <ybhzyz@gmail.com>
2016-11-23 15:29:06 +0800

    Correct and format the script further, previous the FactsSysop upload will have an undefined date.

--- HTML5Application/public_html/Widget_RecentUpload.js
+++ HTML5Application/public_html/Widget_RecentUpload.js
@@ -17,7 +17,7 @@
         ui,li{
         margin:0;
         padding:0;
-        word - wrap: break - word;
+        word-wrap: break-word;
         }
 
         #scrollDiv{
@@ -42,9 +42,9 @@
         #scrollDiv li{
 
 //height: 25px; 
-        padding - left: 10px;
-        font - family: inherit;
-        font - weight: 300;
+        padding-left: 10px;
+        font-family: inherit;
+        font-weight: 300;
         color: inherit; } 
     </style>
 
@@ -53,12 +53,31 @@
 
     var divid = "<!--{$divid}-->";
 
-    function parseDateOrTimevalue(timestamp){
+    function parseDateAndTimevalue(timestamp){
         var t_posi = timestamp.indexOf('T');
         var date = timestamp.slice(0, t_posi);
         var time = timestamp.slice(t_posi + 1, timestamp.length - 1);
         var timevalue = 1 * parseInt(time.slice(0, 2)) + 1 / 60 * parseInt(time.slice(3, 5)) + 1 / 3600 * parseInt(time.slice(6, 8));
         return [date, timevalue];
+    }
+    
+    function pushLiString(list_string, value, user, date){
+        var wikipage_link = "http://factpub.org/wiki/index.php/" + value.title.split(" ").join("_");
+        list_string.push ("<li>" + user + " uploads the paper: " + "<a href=" + wikipage_link + ">" + value.title + "</a>" + " at " + date + "</li>");
+    }
+    
+    function checkFactsSysopUpload(timevalue1, date1, value2){
+        //Check if there is a "new" upload from sysop with time difference less than 10s (1/360 h)
+        var upload = false;
+        var date2 = parseDateAndTimevalue(value2.timestamp)[0];
+        var timevalue2 = parseDateAndTimevalue(value2.timestamp)[1];
+        if (Math.abs(timevalue2 - timevalue1) <= 1 / 360
+            && value2.user === "FactsSysop" 
+            && date2 === date1
+            && value2.type === "new"){
+            upload = true;
+        }
+        return upload;
     }
 
     $.getJSON("http://factpub.org/wiki/api.php?action=query&list=recentchanges&rcprop=title|user|timestamp&rctype=new|edit&rclimit=100&format=json", function (json){
@@ -71,70 +90,57 @@
         while (index < keys_array.length && recent_change_number < 5){
             var key = keys_array[index];
             var value = recent_change_object[key];
-            var user = value.user;
-            
-            //detect upload from register user
-            if (value.ns === 0 && value.type === "edit" && value.user !== "FactsSysop"){
+                       
+            if (value.ns === 0) {
+                var user = value.user;
+                var date = parseDateAndTimevalue(value.timestamp)[0];
+                var timevalue = parseDateAndTimevalue(value.timestamp)[1];
                 
-                var date = parseDateOrTimevalue(value.timestamp)[0];
-                var timevalue = parseDateOrTimevalue(value.timestamp)[1];
-
-                //check the following 5 elements to see if there is a "new" upload from sysop or a modifation to the ACL page with time difference less than 10s (1/360 h)
-                var index_s = index + 1;
-                var limit = index_s + 5;
-                var find_register_upload = false;
-                for (index_s; index_s <= limit && index_s < keys_array.length; index_s++){
-                    var key_s = keys_array[index_s];
+                //detect upload from register user
+                if (value.type === "edit" && value.user !== "FactsSysop"){
+                
+                    //check the previous change to see if it is a modifiction to the correspond ACL page
+                    var find_register_upload = false;  
+                    var key_s = keys_array[index+1];
                     var element = recent_change_object[key_s];
-                    var ele_date = parseDateOrTimevalue(element.timestamp)[0];
-                    var ele_timevalue = parseDateOrTimevalue(element.timestamp)[1];
-                    if (Math.abs(ele_timevalue - timevalue) <= 1 / 360 
-                        && element.user === "FactsSysop" 
-                        && ele_date === date
-                        && element.type === "new"
+                    if (checkFactsSysopUpload(timevalue, date, element)                       
                         && element.title === 'ACL:Page/' + value.title){                            
-                        find_register_upload = true;
-                        break;                        
+                        find_register_upload = true;                     
+                    }
+                    
+                    if (find_register_upload){
+
+                        var next_key = keys_array[index+2];
+                        var next_value = recent_change_object[next_key];
+                    
+                        // if this is the first time upload from the registered user, detect the upload from FactSysop for this upload
+                        // to avoid adding the sysop upload as a recent upload from FactsSysop
+                        if (checkFactsSysopUpload(timevalue, date, next_value)
+                            && next_value.title === value.title) {
+                            index = index + 2;
+                        } else {
+                            index = index + 1;
+                        }
+                        
+                        pushLiString(list_string, value, user, date);                       
+                        recent_change_number ++;
                     }
                 }
-                if (find_register_upload){
-                    var next_key = keys_array[index_s+1];
-                    var next_value = recent_change_object[next_key];
-                    var next_date = parseDateOrTimevalue(next_value.timestamp)[0];
-                    var next_timevalue = parseDateOrTimevalue(next_value.timestamp)[1];
-                    
-                    // if this is the first time upload from the registered user, detect the upload from FactSysop for this upload
-                    if (Math.abs(next_timevalue - timevalue) <= 1 / 360
-                        && next_value.user === "FactsSysop" 
-                        && next_date === date
-                        && next_value.type === "new"
-                        && next_value.title === value.title) {
-                        index = index_s + 1;
-                    } else {
-                        index = index_s;
-                    }
-                    
-                    var wikipage_link = "http://factpub.org/wiki/index.php/" + value.title.split(" ").join("_");
-                    list_string.push ("<li>" + user + " uploads the paper: " + "<a href=" + wikipage_link + ">" + value.title + "</a>" + " at " + date + "</li>");
+
+                //upload from sysop   
+                else if (value.type === "new" && user === "FactsSysop") {
+
+                    user = "Anonymous";
+                    pushLiString(list_string, value, user, date);                    
                     recent_change_number ++;
                 }
-            }
-
-            //upload from sysop   
-            else if (value.type === "new" && user === "FactsSysop" && value.ns === 0){
-
-                user = "Anonymous";
-                var wikipage_link = "http://factpub.org/wiki/index.php/" + value.title.split(" ").join("_");
-                list_string.push ("<li>" + user + " uploads the paper: " + "<a href=" + wikipage_link + ">" + value.title + "</a>" + " at " + date + "</li>");
-                recent_change_number ++;
             }
             index++;
         }
 
         list_string.push("</ul>"); 
-        var list_final_string = list_string.join("");
 
-        document.getElementById(divid).innerHTML = list_final_string;
+        document.getElementById(divid).innerHTML = list_string.join("");
     })
 
     document.head.innerHTML += "<link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet'>";
