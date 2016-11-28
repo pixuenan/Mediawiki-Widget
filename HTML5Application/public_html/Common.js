0d8d25eb460a4905e6038983a602bc06b295ba85
pishijiazu <ybhzyz@gmail.com>
2016-11-28 10:04:44 +0800

    Add the score board button to every page

--- HTML5Application/public_html/Common.js
+++ HTML5Application/public_html/Common.js
@@ -5,13 +5,27 @@
  */
 
 // Highlight the score board link in the navigation bar
-function highlightLink (href) {
-    var els = document.querySelectorAll("a[href='"+ href +"']")[0];
-    els.style.backgroundColor = "#337ab6";
-    els.style.color = "white";
+function creatLinkButton (name, href) {
+    var color = "btn-info";
+    var node = document.createElement("LI"); 
+    var a = document.createElement('A');
+    var linkText = document.createTextNode(name);
+    a.appendChild(linkText);
+    a.className = "btn "+ color +" btn-md active";
+    a.style.color = "white";
+    a.href = href;
+    node.appendChild(a);
+    return node;
 }
 
-$(document).ready(function(){
-    highlightLink ("http://factpub.org/wiki/index.php/Score_Board");
-    })
+var linkButton = creatLinkButton ("Score Board", "http://factpub.org/wiki/index.php/Score_Board");
+var linkButton_collapse = creatLinkButton ("Score Board", "http://factpub.org/wiki/index.php/Score_Board");
+
+var navbar = document.getElementById("navbar");
+var userButton = navbar.getElementsByTagName('ul')[2];
+userButton.insertBefore(linkButton, userButton.childNodes[0]);
+
+var navheader = document.getElementsByClassName("navbar-header")[0];
+var userButton_collaspe = navheader.getElementsByTagName('ul')[1];
+userButton_collaspe.insertBefore(linkButton_collapse, userButton_collaspe.childNodes[0]);
 
