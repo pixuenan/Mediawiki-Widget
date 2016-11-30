81295c462253afb834b67d29247758b2ff6043d2
pishijiazu <ybhzyz@gmail.com>
2016-11-30 14:37:33 +0800

    Track slider

--- HTML5Application/public_html/Widget_SliderDot.js
+++ HTML5Application/public_html/Widget_SliderDot.js
@@ -67,6 +67,9 @@ var slider_func = function() {
 
   
     $('.arrow-next').click(function() {
+        
+        ga('send', 'event', 'slider', 'click', 'sliderdot', 2, false);
+        
         var currentSlide = $('.active-slide');
         var nextSlide = currentSlide.next();
 
@@ -78,31 +81,34 @@ var slider_func = function() {
             nextDot = $('.dot').first();
         }
     
-    currentSlide.fadeOut(600).removeClass('active-slide');
-    nextSlide.fadeIn(600).addClass('active-slide');
+        currentSlide.fadeOut(600).removeClass('active-slide');
+        nextSlide.fadeIn(600).addClass('active-slide');
 
-    currentDot.removeClass('active-dot');
-    nextDot.addClass('active-dot');
+        currentDot.removeClass('active-dot');
+        nextDot.addClass('active-dot');
     });
 
 
-$('.arrow-prev').click(function() {
-    var currentSlide = $('.active-slide');
-    var prevSlide = currentSlide.prev();
+    $('.arrow-prev').click(function() {
 
-    var currentDot = $('.active-dot');
-    var prevDot = currentDot.prev();
-
-    if(prevSlide.length === 0) {
-        prevSlide = $('.slide').last();
-        prevDot = $('.dot').last();
-    }
+        ga('send', 'event', 'dot', 'click', 'slider', 2, false);
     
-    currentSlide.fadeOut(600).removeClass('active-slide');
-    prevSlide.fadeIn(600).addClass('active-slide');
+        var currentSlide = $('.active-slide');
+        var prevSlide = currentSlide.prev();
+        
+        var currentDot = $('.active-dot');
+        var prevDot = currentDot.prev();
 
-    currentDot.removeClass('active-dot');
-    prevDot.addClass('active-dot');
+        if(prevSlide.length === 0) {
+            prevSlide = $('.slide').last();
+            prevDot = $('.dot').last();
+        }
+    
+        currentSlide.fadeOut(600).removeClass('active-slide');
+        prevSlide.fadeIn(600).addClass('active-slide');
+
+        currentDot.removeClass('active-dot');
+        prevDot.addClass('active-dot');
     });
 
 }
